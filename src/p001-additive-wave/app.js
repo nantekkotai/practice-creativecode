b80cac82e6ffdebd40b3b1a4f52aaf36c32830d8
Nobuyuki OSAWA <nantekkotai.nobu@gmail.com>
2016-03-04 18:29:38 +0900

    わけわかんない

--- src/p001-additive-wave/app.js
+++ src/p001-additive-wave/app.js
@@ -1,13 +1,37 @@
+var yoff = 0.0;        // 2nd dimension of perlin noise
+
 function setup() {
-  createCanvas(640, 640);
+  createCanvas(1200, 500);
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
-  }
+  background(51);
 
-  ellipse(mouseX, mouseY, 80, 80);
+  fill(255);
+  // We are going to draw a polygon out of the wave points
+  beginShape(); 
+  
+  var xoff = 1;       // Option #1: 2D Noise
+  var xoff = yoff; // Option #2: 1D Noise
+  
+  // Iterate over horizontal pixels
+  for (var x = 0; x <= width; x += 1) {
+    // Calculate a y value according to noise, map to 
+    
+    // Option #1: 2D Noise
+    var y = map(noise(xoff, yoff), 0, 1, 100,300);
+
+    // Option #2: 1D Noise
+    //var y = map(noise(xoff), 0, 1, 200,300);
+    
+    // Set the vertex
+    vertex(x, y);
+    // Increment x dimension for noise
+    xoff += 0.02;
+  }
+  // increment y dimension for noise
+  yoff += 0.10;
+  vertex(width, height);
+  vertex(0, height);
+  endShape(CLOSE);
 }
